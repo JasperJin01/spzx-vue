a0d44022deac3ba9e0e0df4963ae858c822164b8
Jasper <156559056+JasperJin01@users.noreply.github.com>
2024-12-02 16:24:14 +0800

    添加品牌管理模块及API接口

---
+++ src/api/product/brand.js
@@ -0,0 +1,29 @@
+import request from '@/utils/request'
+
+const prefix = '/admin/product/brand'
+
+// 分页列表
+export const GetBrandPageList = (page, limit) => {
+  return request({
+    url: `${prefix}/${page}/${limit}`,
+    method: 'get'
+  })
+}
+
+// 保存品牌
+export const SaveBrand = brand => {
+  return request({
+    url: `${prefix}/save`,
+    method: 'post',
+    data: brand,
+  })
+}
+
+// 修改信息
+export const UpdateBrandById = brand => {
+  return request({
+    url: `${prefix}/updateById`,
+    method: 'put',
+    data: brand,
+  })
+}
--- src/views/product/brand.vue
+++ src/views/product/brand.vue
@@ -1,3 +1,190 @@
 <template>
-  brand
+  <div>
+    <div class="tools-div">
+      <el-button type="success" size="small" @click="addShow">添 加</el-button>
+    </div>
+
+    <el-table :data="list" style="width: 100%">
+      <el-table-column prop="name" label="品牌名称" />
+      <el-table-column prop="logo" label="品牌图标" #default="scope">
+        <img :src="scope.row.logo" width="50" />
+      </el-table-column>
+      <el-table-column prop="createTime" label="创建时间" />
+      <el-table-column label="操作" align="center" width="200" #default="scope">
+        <el-button type="primary" size="small" @click="editShow(scope.row)">
+          修改
+        </el-button>
+        <el-button type="danger" size="small">删除</el-button>
+      </el-table-column>
+    </el-table>
+
+    <el-pagination
+      :page-sizes="[10, 20, 50, 100]"
+      layout="total, sizes, prev, pager, next"
+      :total="total"
+    />
+
+    <el-dialog v-model="dialogVisible" title="添加或修改" width="30%">
+      <el-form label-width="120px">
+        <el-form-item label="品牌名称">
+          <el-input v-model="brand.name" />
+        </el-form-item>
+        <el-form-item label="品牌图标">
+          <el-upload
+            class="avatar-uploader"
+            action="http://localhost:8501/admin/system/fileUpload"
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :headers="headers"
+          >
+            <img v-if="brand.logo" :src="brand.logo" class="avatar" />
+            <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+          </el-upload>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="saveOrUpdate">提交</el-button>
+          <el-button @click="dialogVisible = false">取消</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
+  </div>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { GetBrandPageList, SaveBrand,UpdateBrandById } from '@/api/product/brand'
+import { ElMessage, ElMessageBox } from 'element-plus'
+import { useApp } from "@/pinia/modules/app";
+
+const headers = {
+  token: useApp().authorization.token,
+}
+
+// 定义提交表单数据模型
+const defaultForm = {
+  id: '',
+  name: '',
+  logo: '',
+}
+const brand = ref(defaultForm)
+const dialogVisible = ref(false)
+
+// 显示添加品牌表单
+const addShow = () => {
+  brand.value = {}
+  dialogVisible.value = true
+}
+
+//上传
+const handleAvatarSuccess = response => {
+  brand.value.logo = response.data
+}
+
+//进入修改
+const editShow = row => {
+  brand.value = {...row}
+  dialogVisible.value = true
+}
+
+// 保存数据
+const saveOrUpdate = () => {
+  if (!brand.value.id) {
+    saveData()
+  } else {
+    updateData()
+  }
+}
+
+// 修改
+const updateData = async () => {
+  await UpdateBrandById(brand.value)
+  dialogVisible.value = false
+  ElMessage.success('操作成功')
+  fetchData()
+}
+
+// 新增
+const saveData = async () => {
+  await SaveBrand(brand.value)
+  dialogVisible.value = false
+  ElMessage.success('操作成功')
+  fetchData()
+}
+
+// 定义表格数据模型
+const list = ref([])
+
+// 分页条数据模型
+const total = ref(0)
+
+//分页条数据模型
+const pageParamsForm = {
+  page: 1, // 页码
+  limit: 10, // 每页记录数
+}
+const pageParams = ref(pageParamsForm)
+
+// 钩子函数
+onMounted(() => {
+  fetchData()
+})
+
+//页面变化
+const handleSizeChange = size => {
+  pageParams.value.limit = size
+  fetchData()
+}
+const handleCurrentChange = number => {
+  pageParams.value.page = number
+  fetchData()
+}
+
+// 分页查询
+const fetchData = async () => {
+  const { code, message, data } = await GetBrandPageList(
+    pageParams.value.page,
+    pageParams.value.limit
+  )
+  list.value = data.list
+  total.value = data.total
+}
+</script>
+
+
+<style scoped>
+.avatar-uploader .avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+</style>
+
+<style>
+.avatar-uploader .el-upload {
+  border: 1px dashed var(--el-border-color);
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  transition: var(--el-transition-duration-fast);
+}
+
+.avatar-uploader .el-upload:hover {
+  border-color: var(--el-color-primary);
+}
+
+.el-icon.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  text-align: center;
+}
+.tools-div {
+  margin: 10px 0;
+  padding: 10px;
+  border: 1px solid #ebeef5;
+  border-radius: 3px;
+  background-color: #fff;
+}
+</style>
